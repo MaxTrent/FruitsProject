be1c2201dda63eaf7b3d25922b0529911c75185d
Samuel Adelowo <samueladelowo92@gmail.com>
2023-03-09 22:25:42 +0100

    code update

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
-const { MongoClient } = require("mongodb");
-// Replace the uri string with your connection string.
-const uri = "<connection string uri>";
-const client = new MongoClient(uri);
-async function run() {
-  try {
-    const database = client.db('sample_mflix');
-    const movies = database.collection('movies');
-    // Query for a movie that has the title 'Back to the Future'
-    const query = { title: 'Back to the Future' };
-    const movie = await movies.findOne(query);
-    console.log(movie);
-  } finally {
-    // Ensures that the client will close when you finish/errors
-    await client.close();
-  }
-}
+// const { MongoClient } = require("mongodb");
+// // Replace the uri string with your connection string.
+// const uri = "<connection string uri>";
+// const client = new MongoClient(uri);
+// async function run() {
+//   try {
+//     const database = client.db('sample_mflix');
+//     const movies = database.collection('movies');
+//     // Query for a movie that has the title 'Back to the Future'
+//     const query = { title: 'Back to the Future' };
+//     const movie = await movies.findOne(query);
+//     console.log(movie);
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
 // run().catch(console.dir);
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://<samueladelowo92>:<>@clustertest.xk26yvm.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
