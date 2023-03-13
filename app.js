c376cfd39717482bad93c7246b3d6f59a965e046
Samuel Adelowo <samueladelowo92@gmail.com>
2023-03-13 23:47:17 +0100

    update

--- app.js
+++ app.js
@@ -1,29 +1,8 @@
-const { MongoClient } = require("mongodb");
+// const { MongoClient } = require("mongodb");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
-// Replace the uri string with your connection string.
-const uri = process.env.connection_string;
-
-const client = new MongoClient(uri);
-
-async function run() {
-  try {
-    const database = client.db('sample_mflix');
-    // const movies = database.collection('movies');
-
-    // Query for a movie that has the title 'Back to the Future'
-    const query = { title: 'Back to the Future' };
-    const movie = await movies.findOne(query);
-
-    console.log(movie);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
-
+mongoose.connect("mongodb://localhost/27017/fruitsDB");
 
 // const { MongoClient, ServerApiVersion } = require('mongodb');
 // const uri = "mongodb+srv://samueladelowo92:sirmuhell@clustertest.xk26yvm.mongodb.net/?retryWrites=true&w=majority";
