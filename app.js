1f45728cb7f7f3bb5592b02d39cee890b04d733d
Samuel Adelowo <samueladelowo92@gmail.com>
2023-03-14 23:25:23 +0100

    update code

--- app.js
+++ app.js
@@ -2,7 +2,14 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-mongoose.connect("mongodb://localhost/27017/fruitsDB");
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+  
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
 
 // const { MongoClient, ServerApiVersion } = require('mongodb');
 // const uri = "mongodb+srv://samueladelowo92:sirmuhell@clustertest.xk26yvm.mongodb.net/?retryWrites=true&w=majority";
